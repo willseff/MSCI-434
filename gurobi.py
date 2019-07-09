0c8d42f1aaf15a9494199f3c9adf73a5362cfd02
Will <w255li@edu.uwaterloo.ca>
2019-07-09 16:59:57 -0400

    Update gurobi.py

--- gurobi.py
+++ gurobi.py
@@ -14,3 +14,26 @@ def eculidian_distance(c,d):
     return answer
 
 print(eculidian_distance(2,3))
+
+trans_distances=[]
+	
+for a in range(9):
+	single_distances = []
+	for b in range(9):
+		single_distances.append([eculidian_distance(a,b)])
+	trans_distances =  trans_distances.append(single_distances)
+
+#df = pd.DataFrame(data, columns=['A', 'B', 'C'])
+
+print(trans_distances)
+
+
+m = Model()
+
+x=m.addVars(location,vtype=GRB.BINARY)
+
+#m.setObjective(quicksum(cost[j]*x[j]for j infood),GRB.MINIMIZE)
+
+
+nut_infood={('Nutrient1','Food1'):2,('Nutrient1','Food2'):0,('Nutrient1','Food3'):3,('Nutrient1','Food4'):1,('Nutrient1','Food5'):2,('Nutrient2','Food1'):0,('Nutrient2','Food2'):1,('Nutrient2','Food3'):2,('Nutrient2','Food4'):2,('Nutrient2','Food5'):1,}
+print(nut_infood[('Nutrient1','Food1')])
