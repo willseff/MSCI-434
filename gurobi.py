0de42b7bcf057ccf0fa7c8934a20b8c6225af793
Will <w255li@edu.uwaterloo.ca>
2019-07-09 17:28:42 -0400

    Working model built

--- gurobi.py
+++ gurobi.py
@@ -2,38 +2,36 @@ from gurobipy import *
 import pandas as pd
 import math
 
-df = pd.read_csv('Assignment 3 Data.csv')
+def distance(a,b):
+  dx = a[0] - b[0]
+  dy = a[1] - b[1]
+  return math.sqrt(dx*dx + dy*dy)
 
-df.x = df.x.astype('float')
-df.y = df.y.astype('float')
 
-def eculidian_distance(c,d):
-    a=int(c)-1
-    b=int(d)-1
-    answer = math.sqrt((df.iloc[a,1]-df.iloc[b,1])**2 + (df.iloc[a,2]-df.iloc[b,2])**2)
-    return answer
+locations = [[15,13],[8,2],[12,6],[9,20], [12,2],[5,19], [6,14], [20,16], [8,16], [18,11]]
+monthly_num_trips = [80,45,75,30,15,75,70,85,10,85]
 
-print(eculidian_distance(2,3))
-
-trans_distances=[]
-	
-for a in range(9):
-	single_distances = []
-	for b in range(9):
-		single_distances.append([eculidian_distance(a,b)])
-	trans_distances =  trans_distances.append(single_distances)
-
-#df = pd.DataFrame(data, columns=['A', 'B', 'C'])
-
-print(trans_distances)
-
+num_locations = len(locations)
 
 m = Model()
 
-x=m.addVars(location,vtype=GRB.BINARY)
+x={}
+n={}
+d={}
 
-#m.setObjective(quicksum(cost[j]*x[j]for j infood),GRB.MINIMIZE)
+for j in range(num_locations):
+  x[j] = m.addVar(vtype=GRB.BINARY)
+  n[j] = monthly_num_trips[j]
 
+for i in range(num_locations):
+  for j in range(num_locations):
+    d[(i,j)] = distance(locations[i], locations[j])
 
-nut_infood={('Nutrient1','Food1'):2,('Nutrient1','Food2'):0,('Nutrient1','Food3'):3,('Nutrient1','Food4'):1,('Nutrient1','Food5'):2,('Nutrient2','Food1'):0,('Nutrient2','Food2'):1,('Nutrient2','Food3'):2,('Nutrient2','Food4'):2,('Nutrient2','Food5'):1,}
-print(nut_infood[('Nutrient1','Food1')])
+m.update()
+
+m.addConstr(quicksum(x[i]for i in range(num_locations)) == 1)
+
+m.setObjective(quicksum(x[i]*d[i,j]*n[j] for i in range(num_locations) for j in range(num_locations)))
+
+m.optimize()
+
