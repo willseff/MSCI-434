830ace2185f6be434a505cf6c18b9842a98d58f9
Will <w255li@edu.uwaterloo.ca>
2019-07-20 11:54:26 -0400

    changed to class

--- Final Project/Data_Generation.py
+++ Final Project/Data_Generation.py
@@ -1,15 +1,20 @@
 import numpy as np
 
-N = 760
-b = np.random.random_integers(0,100,size=(N,N))
-b_symm = (b + b.T)/2
+class symm:
+	def __init__(self, size):
+		N = size
+		b = np.random.random_integers(0,100,size=(N,N))
+		self.b_symm = (b + b.T)/2
 
-for x in np.nditer(b_symm, op_flags = ['readwrite']):
-	if x < 80:
-		x[...] = 0
-	else:
-		x[...] = 1
+		for x in np.nditer(self.b_symm, op_flags = ['readwrite']):
+			if x < 80:
+				x[...] = 0
+			else:
+				x[...] = 1
 
-for x in range(N):
-	b_symm[x][x] = 1
-print(b_symm)
+		for x in range(N):
+			self.b_symm[x][x] = 1
+
+
+	def getMatrix(self):
+		return self.b_symm
