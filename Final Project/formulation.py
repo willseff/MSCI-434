57d60c3db4f9e5ec7a20070f9aedd80be61d51a9
Will <w255li@edu.uwaterloo.ca>
2019-07-20 14:14:19 -0400

    Model finished

--- Final Project/formulation.py
+++ Final Project/formulation.py
@@ -1,4 +1,38 @@
-from Data_Generation import symm
+from Data_Generation import *
+from gurobipy import *
 
-data=symm(760)
-print(data.getMatrix())
+#creates a model where every region is covered with the minimum number of fire stations
+#modeled as a set covering problem
+
+data=symm(100,70)
+sites_covered = data.getMatrix()
+print(sites_covered)
+
+numR = len(sites_covered)
+
+m = Model()
+
+f = {} # Binary variables for each fire station
+r = {} # Binary variable for each region
+
+for i in range(numR):
+	f[i] = m.addVar(vtype=GRB.BINARY, name="fire station %d" % i)
+
+for j in range(numR):
+	r[j] = m.addVar(vtype=GRB.BINARY, name="region covered%d" % j)
+
+m.update()
+
+for j in range(numR):
+	m.addConstr(quicksum(f[i] for i in range(numR) if sites_covered[i][j]==1) >= r[j])
+
+for j in range(numR):
+	m.addConstr(r[j] == 1)
+
+m.setObjective(quicksum(f[i] for i in range(numR)), GRB.MINIMIZE)
+
+m.optimize()
+
+for v in m.getVars():
+
+	print('%s %g' % (v.varName, v.x))
