21edfd3ab66336ddb02841af7767f4551187a8c6
Will <w255li@edu.uwaterloo.ca>
2019-07-29 11:10:18 -0400

    Combines data generation classes into one class with same sizes

--- Final Project/Data_Generation.py
+++ Final Project/Data_Generation.py
@@ -39,4 +39,26 @@ class weightings:
 		return self.weights
 
 
+class data:
+	def __init__ (self,size,rate,demand_mu,demand_sigma,weight_mu,weight_sigma):
+		#create symmetrical matrix
+		N = size
+		b = np.random.random_integers(0,100,size=(N,N))
+		self.b_symm = (b + b.T)/2
+
+		for x in np.nditer(self.b_symm, op_flags = ['readwrite']):
+			if x < rate:
+				x[...] = 0
+			else:
+				x[...] = 1
+
+		for x in range(N):
+			self.b_symm[x][x] = 1
+
+		#create random demand
+		self.demand = np.random.normal(demand_mu, demand_sigma, size)
+		self.demand =np.ceil(self.demand)
+
+		#create random weights
+		self.weights = np.random.normal(weight_mu,demand_sigma,size)
 
